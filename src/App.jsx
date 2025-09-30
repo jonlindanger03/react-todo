ab50bc693675d71e6aab1662f6a30cfb7b570e1d
Jon Lindanger <jonlindanger@Jons-Air.lan>
2025-09-30 09:36:06 +0200

    add and display list

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,43 @@
 
+import { useState } from 'react';
 import './App.css'
 
-function App() {
+export default function App() {
 
   return (
     <>
-      
+      <MyForm/>
     </>
   )
 }
 
-export default App
+let nextId = 0;
+
+function MyForm(){
+  const [todo, setTodo] = useState("")
+  const [todoList, setTodoList] = useState([])
+  
+  function handleclick(){
+    const nextTodo = [
+          ...todoList,
+          {id: nextId++, task: todo,}
+        ];
+
+        setTodoList(nextTodo)
+  }
+
+  return (
+    <>
+      
+        <input type="text" value={todo} onChange={e=> setTodo(e.target.value)} />
+        <button onClick={(handleclick)}>add</button>
+     
+        <ul>
+          {todoList.map(todos => (
+            <li key={todos.id}>{todos.id} {todos.task}</li>
+          ))}
+        </ul>
+
+    </>
+)}
+
